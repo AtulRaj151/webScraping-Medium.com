e333e6655555cfaadfb7d285d5547f73bea0d842
Atul Raj <atulsid151@gmail.com>
2020-10-08 11:56:47 +0530

    Updates

---
+++ precache-manifest.db9d4f4ff3098aaed3b2d3ad3784ceea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59ce4bddc88c7458bd0e3888b7b479d",
+    "url": "/webScraping-Medium.com/index.html"
+  },
+  {
+    "revision": "0c196a4eac8895960455",
+    "url": "/webScraping-Medium.com/static/css/main.0c80568f.chunk.css"
+  },
+  {
+    "revision": "a3998d855d3ba288a91e",
+    "url": "/webScraping-Medium.com/static/js/2.700527f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webScraping-Medium.com/static/js/2.700527f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c196a4eac8895960455",
+    "url": "/webScraping-Medium.com/static/js/main.7983021a.chunk.js"
+  },
+  {
+    "revision": "7e47e344e2f4274f3a74",
+    "url": "/webScraping-Medium.com/static/js/runtime-main.447059ac.js"
+  }
+]);
